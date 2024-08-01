c626ec0d807c8c7bdec7c6df03aea095a67bc741
9SUpran6 <SUpran1999@yandex.ru>
2024-08-02 00:49:12 +0300

    Readers model update

--- src/main/java/model/Readers.java
+++ src/main/java/model/Readers.java
@@ -1,17 +1,19 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Readers {
     private int readerId;
     private String readerName;
+    private List<BookSet> bookSet = new ArrayList<>();
 
     public Readers(){}
 
     public int getId() {
         return readerId;
     }
-
     public void setId(int id) {
         this.readerId = id;
     }
@@ -19,10 +21,10 @@ public class Readers {
     public String getName() {
         return readerName;
     }
+    public void setName(String name) {this.readerName = name;}
 
-    public void setName(String name) {
-        this.readerName = name;
-    }
+    public List<BookSet> getBookSet() {return bookSet;}
+    public void setBookSet(List<BookSet> bookSet) {this.bookSet = bookSet;}
 
     @Override
     public boolean equals(Object o) {
